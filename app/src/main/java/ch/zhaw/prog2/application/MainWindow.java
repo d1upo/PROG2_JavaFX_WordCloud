d43bfa1d1d9ae970c75002991b85e8e3802b4189
lupoden1 <you@example.com>
2022-03-17 19:25:22 +0100

    -

--- app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
@@ -1,13 +1,24 @@
 package ch.zhaw.prog2.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
     }
 
-    private void openMainWindow(Stage stage) {
+    private void openMainWindow(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        Pane rootNode = loader.load();
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }

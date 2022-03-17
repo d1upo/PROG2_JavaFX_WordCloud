3323a599da1247fceb18811aafcf1819b434daf2
lupoden1 <you@example.com>
2022-03-17 16:40:46 +0100

    -

---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
@@ -0,0 +1,13 @@
+package ch.zhaw.prog2.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    private void openMainWindow(Stage stage) {
+    }
+}
